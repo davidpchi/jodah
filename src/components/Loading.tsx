5408edcd2ccd9553b6c041dba67b4f217b6d5587
David <davidpchi@gmail.com>
2024-03-29 10:40:44 -0700

    clean up code some more

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,25 +1,37 @@
-import { Flex } from "@chakra-ui/layout";
 import React from "react";
+
+import { Flex } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/react";
 
 export const Loading = React.memo(function Loading({ text }: { text: string }) {
     return (
         <Flex minHeight="100vh" justify="center" align="start">
             <Flex width="100%" height="100%" justify="center" align="center" flexDirection="column">
-                <img
-                    alt="squirrel gif"
-                    style={{
-                        width: "25%",
-                        height: "25%",
-                        objectFit: "contain",
-                    }}
-                    src={process.env.PUBLIC_URL + "/squirrel.gif"}
-                />
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                    <Spinner
+                        style={{
+                            position: "absolute",
+                            width: "275px",
+                            height: "275px"
+                        }}
+                        size={"xl"}
+                    />
+                    <img
+                        alt="squirrel gif"
+                        style={{
+                            width: "25%",
+                            height: "25%",
+                            objectFit: "contain"
+                        }}
+                        src={process.env.PUBLIC_URL + "/jodah.png"}
+                    />
+                </Flex>
                 <h1
                     style={{
                         color: "black",
                         fontSize: 48,
                         marginRight: 64,
-                        marginLeft: 64,
+                        marginLeft: 64
                     }}
                 >
                     {text}
